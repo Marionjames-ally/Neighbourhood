717d16b6368ce1252a807bbc8eb1d1cc359ae90a
Libb521 <eotieno39@yahoo.com>
2020-03-26 13:41:47 +0300

    created new components so that i can be able to do routing properly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HoodComponent } from './hood/hood.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  (path: 'hood', component: HoodComponent )
+  (path: 'authority', component: AuthorityComponent )
+  (path: 'business', component: BusinessComponent )
+  (path: 'health', component: HealthComponent )
+  (path: 'notifications', component: NotificationsComponent )
+  (path: 'log-out', component: Log-OutComponent )
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MatSliderModule } from '@angular/material/slider';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HoodComponent } from './hood/hood.component';
 import { NavbarComponent } from './navbar/navbar.component';
-// import { HoodComponent } from './components/hood/hood.component';
-// import { AuthorityComponent } from './components/authority/authority.component';
-// import { BusinessComponent } from './components/business/business.component';
-// import { HealthComponent } from './components/health/health.component';
-// import { NotificationsComponent } from './components/notifications/notifications.component';
-// import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AuthorityComponent } from './authority/authority.component';
+import { BusinessComponent } from './business/business.component';
+import { HealthComponent } from './health/health.component';
+import { NotificationComponent } from './notification/notification.component';
+import { LogOutComponent } from './log-out/log-out.component';
 
 
-// const appRoutes: Routes = [
-//   { path: 'hood', component: HoodComponent },
-//   { path: 'authority', component: AuthorityComponent },
-//   { path: 'business', component: BusinessComponent },
-//   { path: 'health', component: HealthComponent },
-//   { path: 'notifications', component: NotificationsComponent },
-//   { path: '**', component: PageNotFoundComponent }
-// ];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HoodComponent,
     NavbarComponent,
+    AuthorityComponent,
+    BusinessComponent,
+    HealthComponent,
+    NotificationComponent,
+    LogOutComponent,
     // HoodComponent,
     // AuthorityComponent,
     // BusinessComponent,
@@ -48,6 +44,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     MatCheckboxModule,
     MatToolbarModule,
     RouterModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
